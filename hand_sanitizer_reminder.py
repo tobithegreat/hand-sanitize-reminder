1ccfdb5258c6f8ac32ffc71c54a113d67ab8b3a1
Oluwatobi Popoola <popoolatobi@gmail.com>
2020-03-04 03:55:38 -0500

    first commit

---
+++ hand_sanitizer_reminder.py
@@ -0,0 +1,17 @@
+import subprocess, time
+
+
+hand_sanitizer_reminder_file = open("hand_sanitizer_reminder", "w+")
+hand_sanitizer_reminder_file.write("HAVE YOU SANITIZED YOUR HANDS, BOY?")
+
+hand_sanitizer_reminder_file.close()
+
+
+
+time_left_on_this_earth = 100000000000000000000000000000000000000000000000000000000000000000000
+
+while time_left_on_this_earth > 0:
+    if time_left_on_this_earth % 10 == 0:
+        subprocess.Popen(['open', '-t', 'hand_sanitizer_reminder'])
+    time.sleep(1)
+    time_left_on_this_earth -= 1
