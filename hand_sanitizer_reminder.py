77b792f11877104c21fc63757b47ed081ecb9102
Oluwatobi Popoola <popoolatobi@gmail.com>
2020-04-23 23:31:08 -0400

    added comment for -t option

--- hand_sanitizer_reminder.py
+++ hand_sanitizer_reminder.py
@@ -12,6 +12,6 @@ time_left_on_this_earth = 100000000000000000000000000000000000000000000000000000
 
 while time_left_on_this_earth > 0:
     if time_left_on_this_earth % 10 == 0:
-        subprocess.Popen(['open', '-t', 'hand_sanitizer_reminder'])
+        subprocess.Popen(['open', '-t', 'hand_sanitizer_reminder']) # -t is the TextEdit application on MacOSX
     time.sleep(1)
     time_left_on_this_earth -= 1
